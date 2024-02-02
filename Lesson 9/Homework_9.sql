3498295a34072672cba7d0417098ed70249a8754
alexandra.liubaya <alexandra.liubaya@gmail.com>
2024-02-03 02:49:00 +0300

    added hw9

--- Lesson 9/Homework_9.sql
+++ Lesson 9/Homework_9.sql
@@ -1,4 +1,4 @@
---People. Поля: ID, FirstName, LastName,  ID_Father, ID_Mother
+--People. РџРѕР»СЏ: ID, FirstName, LastName,  ID_Father, ID_Mother
 
 SELECT 
 	CONCAT(p.LastName, ' ', LEFT(p.FirstName,1), '.', LEFT(pf.FirstName,1), '.') as Son
@@ -6,5 +6,5 @@ SELECT
 FROM People p
 JOIN People pf on p.ID_Father = pf.ID
 JOIN People pg on pf.ID_Father = pg.ID
-WHERE p.FirstName = 'Дмитрий'
+WHERE p.FirstName = 'Р”РјРёС‚СЂРёР№'
 
